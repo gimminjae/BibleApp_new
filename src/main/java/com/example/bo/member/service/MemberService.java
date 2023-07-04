672f527be5a77ac4e728b6c53d7dd55ae16f3a8b
gimminjae <min356812@naver.com>
2023-07-04 16:20:06 +0900

    MemberService 분리

--- src/main/java/com/example/bo/member/service/MemberService.java
+++ src/main/java/com/example/bo/member/service/MemberService.java
@@ -1,14 +1,5 @@
 package com.example.bo.member.service;
 
-import org.springframework.stereotype.Service;
-
-import com.example.bo.member.repository.MemberRepository;
-
-import lombok.RequiredArgsConstructor;
-
-@Service
-@RequiredArgsConstructor
-public class MemberService {
-    private final MemberRepository memberRepository;
+public interface MemberService {
     
 }
