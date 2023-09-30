a4d32a6c87184e9b2f2d0d8e5f7a03ca1de5c3c7
gimminjae <min356812@naver.com>
2023-09-30 14:51:40 +0900

    test sql문 수정

--- src/test/resources/sql/user-repository-test-data.sql
+++ src/test/resources/sql/user-repository-test-data.sql
@@ -1,5 +1,4 @@
 
 -- insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
 -- values (1, 'kok202@naver.com', 'kok202', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'ACTIVE', 0);
-insert into `member` (`memId`, `password`, `email`, `nickname`, `role`, `createDateTime`)
-values ('20230705-b5aa2484b8b345d586804fef824bbd8e', '$2a$10$mnWEqJxaRi6UgWn.fBXHv.NqHAKBqUaWKUXJ3op00VJLuGWh/Ir1e', 'testmail@naver.com', 'nickname', 'MEMBER', now());
+insert into `member` (`mem_id`, `password`, `email`, `nickname`, `role`, `create_date_time`) values ('20230705-b5aa2484b8b345d586804fef824bbd8e', '$2a$10$mnWEqJxaRi6UgWn.fBXHv.NqHAKBqUaWKUXJ3op00VJLuGWh/Ir1e', 'testmail@naver.com', 'nickname', 'MEMBER', now());
